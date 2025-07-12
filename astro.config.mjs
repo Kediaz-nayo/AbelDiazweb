4cf293525816d2562c98986ff02012d9685a1939
kediaz-nayo <asesoramientojuanista@gmail.com>
2025-07-12 12:53:22 -0600

    feat: configure netlify adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify'; // <-- 1. IMPORTA EL ADAPTADOR
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // <-- 2. AÑADE ESTA LÍNEA CRUCIAL
+  adapter: netlify(), // <-- 3. AÑADE ESTA LÍNEA
+});
