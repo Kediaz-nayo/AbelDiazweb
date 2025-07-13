7b8319f8d23682c67f8c77066be87aa88e822db4
kediaz-nayo <asesoramientojuanista@gmail.com>
2025-07-12 22:21:32 -0600

    fix: Corregir astro.config.js para habilitar SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Importamos el adaptador de Netlify
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  // ESTA ES LA ÚNICA LÍNEA QUE NECESITAMOS AÑADIR
+  output: 'server', 
+  
   adapter: netlify()
 });
