34a936ab25010194d80d1392bfd5b26e83aed321
Kediaz-nayo <asesoramientojuanista@gmail.com>
2025-09-21 09:49:09 -0600

    astro.config.mjs file to get a sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,16 @@ import { defineConfig } from 'astro/config';
 // Importamos el adaptador de Netlify
 import netlify from '@astrojs/netlify';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  // Add your site's URL here
+  site: 'https://www.abeldiaz.pro', // ⚠️ Replace this with your actual domain
+
   // ESTA ES LA ÚNICA LÍNEA QUE NECESITAMOS AÑADIR
-  output: 'server', 
-  
-  adapter: netlify()
+  output: 'server',
+
+  adapter: netlify(),
+  integrations: [sitemap()]
 });
